0e9e024330b93fde11b2087a626d092535b52e81
sandroteste72 <sandro.teste72@gmail.com>
2021-09-01 22:53:06 -0300

    Correction classes

--- src/main/java/com/digitalinnovationone/live/controller/CompanyController.java
+++ src/main/java/com/digitalinnovationone/live/controller/CompanyController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
-@RequestMapping("/journey")
+@RequestMapping("/company")
 public class CompanyController {
 
     @Autowired
